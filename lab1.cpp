c3a4ff23874ed4dcc90f167dedc922e6d26c980f
KrystianPi <krystek.pietrzak@gmail.com>
2020-11-02 09:47:21 +0000

    solved

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <math.h>
+using namespace std;
+
+class Wektor2D
+{
+public:
+    Wektor2D()
+    {
+        setX(0);
+        setY(0);
+    }
+    double norm(double a, double b) const
+    {
+        double vector_norm = sqrt(a * a + b * b);
+        cout << "norm = " << vector_norm << endl;
+        return vector_norm;
+    }
+    void print() { cout << "x = " << x << " y = " << y << endl; }
+    Wektor2D(double aa, double bb)
+    {
+        x = aa;
+        y = bb;
+        print();
+        norm(x, y);
+    }
+    double getX() { return x; }
+    double getY() { return y; }
+    void   setX(double f) { x = f; }
+    void   setY(double h) { y = h; }
+    ~Wektor2D()
+    {
+        cout << "tu destruktor" << endl;
+        print();
+    }
+    Wektor2D operator+(Wektor2D& w1) const { return Wektor2D(w1.getX() + x, w1.getY() + y); }
+    Wektor2D operator*(Wektor2D& w2) const { return Wektor2D(w2.getX() * x, w2.getY() * y); }
+    
+
+private:
+    double x;
+    double y;
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D v1, v2{3, 5};
+    v2.setX(2);
+    v1.setX(3);
+    v1.setY(3);
+    v2.print();
+    v1.print();
+    Wektor2D v3 = v1 + v2;
+    Wektor2D v4 = v3 * v1;
+    v3.print();
+    v4.print();
 }
--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,42 @@
-class Wektor2D
-{
-    // Tutaj napisz implementacje klasy Wektor2D
+class Wektor2D{
+    public:
+    Wektor2D(){
+        x = 0;
+        y = 0;
+    }
+    Wektor2D(double aa, double bb)
+    {
+        x = aa;
+        y = bb;
+    }
+    double getX(){
+        return x;
+    }
+    double getY(){
+        return y;
+    }
+    void setX(double a){
+        x=a;
+    }
+    void setY(double b){
+        y=b;
+    }
+    friend Wektor2D operator+(Wektor2D& w1 , Wektor2D& w2) { 
+        return Wektor2D(w1.getX() + w2.getX(), w1.getY() + w2.getY()); 
+        }
+    ~Wektor2D(){
+        print();
+        cout << "tu destruktor" << endl;
+    }
+    void print(){
+        cout << "x = " << x << " y = " << y << endl;
+    }
+    friend double operator*( Wektor2D& w1,  Wektor2D& w2) { 
+        double prod = w1.x * w2.x + w1.y * w2.y;
+        return prod;
+    }
+    private:
+    double x;
+    double y;
 };
+    
